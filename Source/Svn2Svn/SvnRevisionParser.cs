f2faabd2ee5653ecbc5f91378ddc9d921c09b012
Daniel Doubrovkine <dblock@dblock.org>
2010-12-24 19:36:35 +0000

    Committing missing file.
    --HG--
    extra : convert_revision : svn%3A9b283d60-5439-405e-af05-b73fd8c4d996/trunk%4059161

---
+++ Source/Svn2Svn/SvnRevisionParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SharpSvn;
+
+namespace Svn2Svn
+{
+    abstract class SvnRevisionParser
+    {
+        public static SvnRevision Parse(string revision)
+        {
+            String s = revision.ToLower();
+            switch(s)
+            {
+                case "head":
+                    return SvnRevision.Head;
+                case "base":
+                    return SvnRevision.Base;
+                case "none":
+                    return SvnRevision.None;
+                case "one":
+                    return SvnRevision.One;
+                default:
+                    long i = 0;
+                    if (! long.TryParse(s, out i) || i < 0) 
+                    {
+                        throw new Exception(string.Format("Invalid revision '{0}', must be a positive number, ONE, HEAD, etc.", 
+                            revision));
+                    }
+                    return new SvnRevision(i);
+            }            
+        }
+    }
+}
